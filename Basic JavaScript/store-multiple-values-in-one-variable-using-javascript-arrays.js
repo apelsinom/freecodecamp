9d88f019ffb775ce800102fbb07e8ce92d6acd5d
apelsinom <apelsinom@mail.ru>
2023-08-29 23:57:30 +0300

    Store Multiple Values in one Variable using JavaScript Arrays

---
+++ Basic JavaScript/store-multiple-values-in-one-variable-using-javascript-arrays.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION:
+// Modify the new array myArray so that it contains
+// both a string and a number (in that order).
+
+// ✅ SOLUTION:
+// Only change code below this line
+const myArray = ['rum', 2];
+console.log(myArray)
