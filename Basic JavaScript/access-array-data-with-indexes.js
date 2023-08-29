f7905c6ac16da5b0465f22b2e11ada3703cfbf31
apelsinom <apelsinom@mail.ru>
2023-08-30 00:11:23 +0300

    Access Array Data with Indexes

---
+++ Basic JavaScript/access-array-data-with-indexes.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION:
+// Create a variable called myData and set it to equal the first
+// value of myArray using bracket notation.
+
+// ✅ SOLUTION:
+const myArray = [50, 60, 70];
+let myData = myArray[0];
+console.log(myData)
