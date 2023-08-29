9509cce4bfebe24a6e04e977e029af830908d4a4
apelsinom <apelsinom@mail.ru>
2023-08-29 16:33:54 +0300

    Declare String Variables

---
+++ Basic JavaScript/declare-string-variables.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+// Create two new string variables: myFirstName and myLastName and
+// assign them the values of your first and last name, respectively.
+
+// ✅ SOLUTION:
+let myFirstName = 'Sergey';
+let myLastName = 'Sidorenko';
+console.log(myFirstName);
+console.log(myLastName);
