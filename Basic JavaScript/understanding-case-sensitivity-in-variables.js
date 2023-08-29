0c8510b800f25e83f8baca35d8231b7b20745c04
apelsinom <apelsinom@mail.ru>
2023-08-29 17:03:35 +0300

    Understanding Case Sensitivity in Variables

---
+++ Basic JavaScript/understanding-case-sensitivity-in-variables.js
@@ -0,0 +1,14 @@
+// ❓ DESCRIPTION:
+// Modify the existing declarations and assignments so their names use camelCase.
+// Do not create any new variables.
+
+// ✅ SOLUTION:
+// Variable declarations
+let studlyCapVar;
+let properCamelCase;
+let titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
