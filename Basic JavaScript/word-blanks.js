0177407f3de5e4e8999216596a316155d676f2e4
apelsinom <apelsinom@mail.ru>
2023-08-29 23:51:49 +0300

    Word Blanks

---
+++ Basic JavaScript/word-blanks.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+// You will need to use the string concatenation operator + to build a new string,
+// using the provided variables: myNoun, myAdjective, myVerb, and myAdverb.
+// You will then assign the formed string to the wordBlanks variable.
+// You should not change the words assigned to the variables.
+
+// ✅ SOLUTION:
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb; // Change this line
+// Only change code above this line
+console.log(wordBlanks)
