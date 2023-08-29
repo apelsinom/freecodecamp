69d459b9328f9b380492d78c31538e689bbcedb7
apelsinom <apelsinom@mail.ru>
2023-08-29 15:04:22 +0300

    Assigning the Value of One Variable to Another - delete var

---
+++ Basic JavaScript/initializing-variables-with-the-assignment-operator.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION:
+// It is common to initialize a variable to an initial
+// value in the same line as it is declared.
+// Define e variable a with var and initialize it to a value of 9.
+
+// ✅ SOLUTION:
+let e = 9;
+console.log(e);
