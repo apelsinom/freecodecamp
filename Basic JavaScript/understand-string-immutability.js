a74ac79b979c25b2c9cfc9c39231069c104a4879
apelsinom <apelsinom@mail.ru>
2023-08-29 23:21:13 +0300

    Understand String Immutability

--- Basic JavaScript/understand-string-immutability.js
+++ Basic JavaScript/understand-string-immutability.js
@@ -1,6 +1,6 @@
 // ❓ DESCRIPTION:
-Correct the assignment to myStr so it contains the string value of Hello World
-using the approach shown in the example above.
+//Correct the assignment to myStr, so it contains the string value of Hello World
+//using the approach shown in the example above.
 
 // ✅ SOLUTION:
 // Setup
