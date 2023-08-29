5ca3f343e37d319c53ba2aefbcbc62581494ebaf
apelsinom <apelsinom@mail.ru>
2023-08-29 17:09:10 +0300

    Explore Differences Between the var and let Keywords

---
+++ Basic JavaScript/explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION:
+// If you replace var with let in the code above, it results in an error:
+
+// ✅ SOLUTION:
+let catName = "Oliver";
+let catSound = "Meow!";
+console.log(catName);
+console.log(catSound);
