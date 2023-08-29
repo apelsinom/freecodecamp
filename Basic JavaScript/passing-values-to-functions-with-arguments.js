281df947edae5ece377d95238865d46ced879188
apelsinom <apelsinom@mail.ru>
2023-08-30 00:58:36 +0300

    Passing Values to Functions with Arguments

---
+++ Basic JavaScript/passing-values-to-functions-with-arguments.js
@@ -0,0 +1,10 @@
+// ❓ DESCRIPTION:
+// Create a function called functionWithArgs that accepts two
+// arguments and outputs their sum to the dev console.
+// Call the function with two numbers as arguments.
+
+// ✅ SOLUTION:
+function functionWithArgs(param1, param2) {
+  console.log(param1 + param2);
+}
+functionWithArgs(4, 7);
