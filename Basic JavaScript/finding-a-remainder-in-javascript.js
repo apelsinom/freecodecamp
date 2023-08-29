e92de74194c6488674880b004fb3821724046b19
apelsinom <apelsinom@mail.ru>
2023-08-29 18:04:34 +0300

    Finding a Remainder in JavaScript

---
+++ Basic JavaScript/finding-a-remainder-in-javascript.js
@@ -0,0 +1,7 @@
+// ❓ DESCRIPTION:
+// Set remainder equal to the remainder of 11 divided by 3
+// using the remainder (%) operator.
+
+// ✅ SOLUTION:
+const remainder = 11 % 3;
+console.log(remainder)
