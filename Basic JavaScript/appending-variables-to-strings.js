dc7ff7f093a3acc14a37997cfe7815934fd43687
apelsinom <apelsinom@mail.ru>
2023-08-29 22:01:20 +0300

    Appending Variables to Strings

---
+++ Basic JavaScript/appending-variables-to-strings.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+// Set someAdjective to a string of at least 3 characters and
+// append it to myStr using the += operator.
+
+// ✅ SOLUTION:
+// Change code below this line
+const someAdjective = "very well";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
