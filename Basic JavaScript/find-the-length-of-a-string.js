0218954ff142445f33c09b2eb5785c279e812b2a
apelsinom <apelsinom@mail.ru>
2023-08-29 22:58:52 +0300

    Find the Length of a String

---
+++ Basic JavaScript/find-the-length-of-a-string.js
@@ -0,0 +1,10 @@
+// ❓ DESCRIPTION:
+// Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// ✅ SOLUTION:
+// Setup
+let lastNameLength;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
