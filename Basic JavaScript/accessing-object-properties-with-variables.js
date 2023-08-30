86d00aa815236d9085d9b6b55ee69b8cbb826eb5
apelsinom <apelsinom@mail.ru>
2023-08-30 19:57:59 +0300

    Accessing Object Properties with Variables

---
+++ Basic JavaScript/accessing-object-properties-with-variables.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+// Set the playerNumber variable to 16.
+// Then, use the variable to look up the player's name and assign it to player.
+
+// ✅ SOLUTION:
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+console.log(player)
