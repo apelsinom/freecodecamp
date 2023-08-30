38847ae1fc5dc368d6d8181320ae41d0a5a1963c
apelsinom <apelsinom@mail.ru>
2023-08-30 12:18:43 +0300

    Comparison with the Strict Equality Operator

---
+++ Basic JavaScript/comparison-with-the-strict-equality-operator.js
@@ -0,0 +1,14 @@
+// ❓ DESCRIPTION:
+// Use the strict equality operator in the if statement
+// so the function will return the string Equal when val is strictly equal to 7.
+
+// ✅ SOLUTION:
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
