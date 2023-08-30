62f9c8f4f60d66007af6b9762b1fd562e172b19a
apelsinom <apelsinom@mail.ru>
2023-08-30 16:07:47 +0300

    Returning Boolean Values from Functions

---
+++ Basic JavaScript/returning-boolean-values-from-functions.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION:
+// Fix the function isLess to remove the if/else statements.
+
+// ✅ SOLUTION:
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
