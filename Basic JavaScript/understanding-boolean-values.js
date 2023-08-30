712181c4f8fc46011f1f2a5e9050a5d116da2f6c
apelsinom <apelsinom@mail.ru>
2023-08-30 11:29:55 +0300

    Understanding Boolean Values

---
+++ Basic JavaScript/understanding-boolean-values.js
@@ -0,0 +1,13 @@
+// ❓ DESCRIPTION:
+// Modify the welcomeToBooleans function so that it returns true instead of false.
+
+// ✅ SOLUTION:
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+console.log(welcomeToBooleans())
