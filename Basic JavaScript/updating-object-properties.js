0df647eef4c6ee37f5503080d70f4fa9fb5f2116
apelsinom <apelsinom@mail.ru>
2023-08-30 20:15:15 +0300

    Updating Object Properties

---
+++ Basic JavaScript/updating-object-properties.js
@@ -0,0 +1,15 @@
+// ❓ DESCRIPTION:
+// Update the myDog object's name property.
+// Let's change her name from Coder to Happy Coder.
+// You can use either dot or bracket notation.
+
+// ✅ SOLUTION:
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+// Only change code below this line
+myDog.name = 'Happy Coder';
