6082ffeb2c85f509eb30b285df4a262035ec40ca
apelsinom <apelsinom@mail.ru>
2023-08-30 12:32:20 +0300

    Comparison with the Greater Than Operator

---
+++ Basic JavaScript/comparison-with-the-greater-than-operator.js
@@ -0,0 +1,18 @@
+// ❓ DESCRIPTION:
+// Add the greater than operator to the indicated
+// lines so that the return statements make sense.
+
+// ✅ SOLUTION:
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
