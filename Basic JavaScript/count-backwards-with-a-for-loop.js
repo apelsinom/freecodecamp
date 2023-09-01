29a832b2ded63918fb583f419454514306f169d1
apelsinom <apelsinom@mail.ru>
2023-09-01 22:54:48 +0300

    Iterate Through an Array with a For Loop

---
+++ Basic JavaScript/count-backwards-with-a-for-loop.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION:
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+
+// ✅ SOLUTION:
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i >= 1; i -= 2) {
+  myArray.push(i);
+}
