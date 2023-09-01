39341aa3b6dde9ad9260c768622099b579191654
apelsinom <apelsinom@mail.ru>
2023-09-01 21:21:23 +0300

    Iterate with JavaScript While Loops

---
+++ Basic JavaScript/iterate-with-javascript-while-loops.js
@@ -0,0 +1,15 @@
+// ❓ DESCRIPTION:
+// Add the numbers 5 through 0 (inclusive) in descending order
+// to myArray using a while loop.
+
+// ✅ SOLUTION:
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
