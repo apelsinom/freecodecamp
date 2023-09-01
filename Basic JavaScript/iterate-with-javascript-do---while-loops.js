2f484bb2f76c074f5ddb922265ed83d4d997e8bd
apelsinom <apelsinom@mail.ru>
2023-09-01 23:17:31 +0300

    Iterate with JavaScript Do...While Loops

---
+++ Basic JavaScript/iterate-with-javascript-do---while-loops.js
@@ -0,0 +1,15 @@
+// ❓ DESCRIPTION:
+// Change the while loop in the code to a do...while
+// loop so the loop will push only the number 10 to myArray,
+// and i will be equal to 11 when your code has finished running.
+
+// ✅ SOLUTION:
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5) 
