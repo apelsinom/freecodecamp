1e39387b5117f099a40b17441cfe8710ec1596b8
apelsinom <apelsinom@mail.ru>
2023-09-01 21:42:54 +0300

    Iterate with JavaScript For Loops

---
+++ Basic JavaScript/iterate-with-javascript-for-loops.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION:
+// Use a for loop to push the values 1 through 5 onto myArray.
+
+// ✅ SOLUTION:
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
